37b4789bd89ba32a2a623fde52d37d6cf80ac07a
Cristina Gonzalez <crisgruiz@protonmail.com>
2021-06-02 16:09:22 +0200

    add hours and prices info

--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,45 @@ let date = new Date();
 let day = date.getDate();
 let month = date.getMonth() + 1;
 let year = date.getFullYear();
-
 //Llamada al API
-const getDataFromApi = () => {
+let info = null;
+
+const getElectricityPrice = () => {
   return fetch(
-    `https://api.esios.ree.es/archives/70/download_json?locale=es&date=${day}-${month}-${year}`
-  ).then((response) => response.json());
+    `https://api.esios.ree.es/archives/70/download_json?locale=es&date=${year}-${month}-${day}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      info = data;
+      paintHours();
+      paintPrices();
+    });
+};
+getElectricityPrice();
+
+const infoDetail = document.querySelector(".js-table");
+const hours = document.querySelector(".js-hour");
+const prices = document.querySelector(".js-price");
+
+const paintHours = () => {
+  let htmlCode = "";
+  for (let i = 0; i < info.PVPC.length; i++) {
+    htmlCode += `<p> ${info.PVPC[i].Hora}</p>`;
+  }
+  hours.innerHTML = htmlCode;
+};
+
+const paintPrices = () => {
+  let htmlCode = "";
+  for (let i = 0; i < info.PVPC.length; i++) {
+    const prices = parseInt(info.PVPC[i].PCB);
+    const correctPrices = prices / 1000;
+    htmlCode += `<p> ${correctPrices} cént</p>`;
+  }
+  prices.innerHTML = htmlCode;
+};
+
+const paintNoData = () => {
+  let htmlCode = "Loading";
+  infoDetail.innerHTML = htmlCode;
 };
