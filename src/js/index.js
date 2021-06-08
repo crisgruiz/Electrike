b23bdf498b2676170484302bc50633abf1eefc34
Cristina Gonzalez <crisgruiz@protonmail.com>
2021-06-08 12:08:16 +0200

    add function of switching resume prices

--- src/js/index.js
+++ src/js/index.js
@@ -25,8 +25,10 @@ const getElectricityPrice = () => {
       });
     })
     .then((hourlyPrices) => {
-      paintMinPrice(hourlyPrices);
-      paintMaxPrice(hourlyPrices);
+      paintMinPricePCB(hourlyPrices);
+      paintMaxPricePCB(hourlyPrices);
+      paintMinPriceCYM(hourlyPrices);
+      paintMaxPriceCYM(hourlyPrices);
       paintHours(hourlyPrices);
       paintPricesPCB(hourlyPrices);
       paintPricesCYM(hourlyPrices);
@@ -41,55 +43,6 @@ const variationPCB = document.querySelector(".js-varPCB");
 const hoursCYM = document.querySelector(".js-hourCYM");
 const pricesCYM = document.querySelector(".js-priceCYM");
 const variationCYM = document.querySelector(".js-varCYM");
-const lowPrice = document.querySelector(".js-low");
-const highPrice = document.querySelector(".js-high");
-
-//Resume prices
-
-const paintMinPrice = (hourlyPrices) => {
-  let htmlCode = "";
-  let minElement = { PCB: 1000 };
-  for (let i = 0; i < hourlyPrices.length; i++) {
-    if (hourlyPrices[i].PCB < minElement.PCB) {
-      minElement = hourlyPrices[i];
-    }
-  }
-  let minElementCYM = { CYM: 1000 };
-  for (let i = 0; i < hourlyPrices.length; i++) {
-    if (hourlyPrices[i].CYM < minElementCYM.CYM) {
-      minElementCYM = hourlyPrices[i];
-    }
-  }
-
-  htmlCode += `<p class="priceTitle">Hora más barata</p>`;
-  htmlCode += `<p class="place">Península, Canarias y Baleares:</p>`;
-  htmlCode += `<p> ${minElement.hour} -> ${minElement.PCB} €/kWh</p>`;
-  htmlCode += `<p class="place">Ceuta y Melilla:</p>`;
-  htmlCode += `<p> ${minElement.hour} -> ${minElementCYM.CYM} €/kWh</p>`;
-  lowPrice.innerHTML = htmlCode;
-};
-
-const paintMaxPrice = (hourlyPrices) => {
-  let htmlCode = "";
-  let maxElement = { PCB: 0 };
-  for (let i = 0; i < hourlyPrices.length; i++) {
-    if (hourlyPrices[i].PCB > maxElement.PCB) {
-      maxElement = hourlyPrices[i];
-    }
-  }
-  let maxElementCYM = { CYM: 0 };
-  for (let i = 0; i < hourlyPrices.length; i++) {
-    if (hourlyPrices[i].CYM > maxElementCYM.CYM) {
-      maxElementCYM = hourlyPrices[i];
-    }
-  }
-  htmlCode += `<p class="priceTitle">Hora más cara</p>`;
-  htmlCode += `<p class="place">Península, Canarias y Baleares:</p>`;
-  htmlCode += `<p> ${maxElement.hour} -> ${maxElement.PCB} €/kWh</p>`;
-  htmlCode += `<p class="place">Ceuta y Melilla:</p>`;
-  htmlCode += `<p> ${maxElement.hour} -> ${maxElementCYM.CYM} €/kWh</p>`;
-  highPrice.innerHTML = htmlCode;
-};
 
 //table with data
 
--- src/js/table.js
+++ src/js/table.js
@@ -1,8 +1,73 @@
+const lowPricePCB = document.querySelector(".js-lowPCB");
+const highPricePCB = document.querySelector(".js-highPCB");
+const lowPriceCYM = document.querySelector(".js-lowCYM");
+const highPriceCYM = document.querySelector(".js-highCYM");
+
+//Resume prices
+
+const paintMinPricePCB = (hourlyPrices) => {
+  let htmlCode = "";
+  let minElement = { PCB: 1000 };
+  for (let i = 0; i < hourlyPrices.length; i++) {
+    if (hourlyPrices[i].PCB < minElement.PCB) {
+      minElement = hourlyPrices[i];
+    }
+  }
+
+  htmlCode += `<p class="priceTitle">Hora más barata</p>`;
+  htmlCode += `<p> ${minElement.hour} -> ${minElement.PCB} €/kWh</p>`;
+  lowPricePCB.innerHTML = htmlCode;
+};
+
+const paintMinPriceCYM = (hourlyPrices) => {
+  let htmlCode = "";
+  let minElementCYM = { CYM: 1000 };
+  for (let i = 0; i < hourlyPrices.length; i++) {
+    if (hourlyPrices[i].CYM < minElementCYM.CYM) {
+      minElementCYM = hourlyPrices[i];
+    }
+  }
+
+  htmlCode += `<p class="priceTitle">Hora más barata</p>`;
+  htmlCode += `<p> ${minElementCYM.hour} -> ${minElementCYM.CYM} €/kWh</p>`;
+  lowPriceCYM.innerHTML = htmlCode;
+};
+
+const paintMaxPricePCB = (hourlyPrices) => {
+  let htmlCode = "";
+  let maxElement = { PCB: 0 };
+  for (let i = 0; i < hourlyPrices.length; i++) {
+    if (hourlyPrices[i].PCB > maxElement.PCB) {
+      maxElement = hourlyPrices[i];
+    }
+  }
+
+  htmlCode += `<p class="priceTitle">Hora más cara</p>`;
+  htmlCode += `<p> ${maxElement.hour} -> ${maxElement.PCB} €/kWh</p>`;
+  highPricePCB.innerHTML = htmlCode;
+};
+
+const paintMaxPriceCYM = (hourlyPrices) => {
+  let htmlCode = "";
+  let maxElementCYM = { CYM: 0 };
+  for (let i = 0; i < hourlyPrices.length; i++) {
+    if (hourlyPrices[i].CYM > maxElementCYM.CYM) {
+      maxElementCYM = hourlyPrices[i];
+    }
+  }
+  htmlCode += `<p class="priceTitle">Hora más cara</p>`;
+  htmlCode += `<p> ${maxElementCYM.hour} -> ${maxElementCYM.CYM} €/kWh</p>`;
+  highPriceCYM.innerHTML = htmlCode;
+};
+
 const tabsItem = Array.prototype.slice.apply(
   document.querySelectorAll(".tabs__item")
 );
 const tablesItem = Array.prototype.slice.apply(
   document.querySelectorAll(".table__item")
+);
+const resumeItem = Array.prototype.slice.apply(
+  document.querySelectorAll(".resume__item")
 );
 const tabs = document.getElementById("tabs");
 
@@ -13,6 +78,8 @@ const handleClick = (e) => {
     tabsItem[i].classList.add("active");
     tablesItem.map((table) => table.classList.remove("active"));
     tablesItem[i].classList.add("active");
+    resumeItem.map((resume) => resume.classList.remove("active"));
+    resumeItem[i].classList.add("active");
   }
 };
 
