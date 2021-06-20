30d58577c2df1ce16d8526a4bffb6596acfa2a04
Cristina Gonzalez <crisgruiz@protonmail.com>
2021-06-20 14:08:23 +0200

    add color base on price sections

--- src/js/index.js
+++ src/js/index.js
@@ -33,7 +33,7 @@ const highPriceCYM = document.querySelector(".js-highCYM");
 //Llamada al API
 const getElectricityPrice = () => {
   return fetch(
-    `https://electrike-otkzylkdbx.s3-eu-west-1.amazonaws.com/v2/${formatDate}.json`
+    `https://electrike-otkzylkdbx.s3-eu-west-1.amazonaws.com/v2/2021-06-15.json`
   )
     .then((response) => response.json())
     .then((hourlyPrices) => {
@@ -61,30 +61,40 @@ getElectricityPrice();
 const paintHours = (hourlyPrices) => {
   let htmlCode = "";
   for (let i = 0; i < hourlyPrices.length; i++) {
-    htmlCode += `<p class="table__hour--item"> ${hourlyPrices[i].hour}:00</p>`;
+    sectionStyle = sectionsMap[hourlyPrices[i].section];
+    htmlCode += `<p class="table__hour--item ${sectionStyle}"> ${hourlyPrices[i].hour}:00</p>`;
   }
   hoursPCB.innerHTML = htmlCode;
   hoursCYM.innerHTML = htmlCode;
 };
 
+const sectionsMap = {
+  v: "sectionLow",
+  ll: "sectionMedium",
+  p: "sectionHigh",
+};
+
 const paintPrices = (hourlyPrices, prices) => {
   let htmlCode = "";
   for (let i = 0; i < hourlyPrices.length; i++) {
-    htmlCode += `<p class="table__price--item"> ${hourlyPrices[i].price} <span class="units">€/kWh</span></p>`;
+    sectionStyle = sectionsMap[hourlyPrices[i].section];
+    htmlCode += `<p class="table__price--item ${sectionStyle}"> ${hourlyPrices[i].price} <span class="units">€/kWh</span></p>`;
   }
   prices.innerHTML = htmlCode;
 };
 
 const paintVariation = (hourlyPrices, variation) => {
-  let htmlCode = `<p class="table__var--icon equal"><i class="fas fa-equals"></i></p>`;
+  let htmlCode = `<p class="table__var--icon equal sectionLow"><i class="fas fa-equals"></i></p>`;
   for (let i = 1; i < hourlyPrices.length; i++) {
+    sectionStyle = sectionsMap[hourlyPrices[i].section];
+    htmlCode += `<p class="table__var--icon ${sectionStyle}`;
     let previousValue = hourlyPrices[i - 1].price;
     if (previousValue < hourlyPrices[i].price) {
-      htmlCode += `<p class="table__var--icon up"><i class="fas fa-chevron-circle-up"></i></p>`;
+      htmlCode += ` up"><i class="fas fa-chevron-circle-up"></i></p>`;
     } else if (previousValue > hourlyPrices[i].price) {
-      htmlCode += `<p class="table__var--icon down"><i class="fas fa-chevron-circle-down"></i></p>`;
+      htmlCode += ` down"><i class="fas fa-chevron-circle-down"></i></p>`;
     } else {
-      htmlCode += `<p class="table__var--icon equal"><i class="fas fa-equals"></i></p>`;
+      htmlCode += ` equal"><i class="fas fa-equals"></i></p>`;
     }
   }
   variation.innerHTML = htmlCode;
